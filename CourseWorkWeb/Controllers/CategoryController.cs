68e0aff36f4eae4135f30d8bd803eab55258db7b
vladyslav-keidaliuk <vladislavdocuments@gmail.com>
2023-10-30 00:05:45 +0200

    Without Repository Pattern, with Context only at the moment, add notifications, change site.css on custom, Added views and actions to create | delete | edit category.

--- CourseWorkWeb/Controllers/CategoryController.cs
+++ CourseWorkWeb/Controllers/CategoryController.cs
@@ -19,5 +19,102 @@ namespace CourseWorkWeb.Controllers
 
             return View(CategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            // Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            // Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            // if (category.Name == category.DisplayOrder.ToString())
+            // {
+            //     ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            // }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            // Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            // Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+    
+        }
     }
 }
--- CourseWorkWeb/Models/Category.cs
+++ CourseWorkWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseWorkWeb.Models;
 
@@ -7,6 +8,10 @@ public class Category
     [Key] // Primary key
     public int Id { get; set; }
     [Required]
+    [MaxLength(30)]
+    [DisplayName("Category Name")]
     public string Name { get; set; }
+    [DisplayName("Display Order")]
+    [Range(1,100,ErrorMessage = "Display Order must be between 1-100")]
     public int DisplayOrder { get; set; }
 }
