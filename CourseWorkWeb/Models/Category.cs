3d6fd2bd9f9790aab6a11a6c74211f42a52cf0d3
vladyslav-keidaliuk <vladislavdocuments@gmail.com>
2023-10-29 20:05:55 +0200

    Add DbContext, migration, directory for Views of CategoryController , add castom theme of bootstrap

---
+++ CourseWorkWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseWorkWeb.Models;
+
+public class Category
+{
+    [Key] // Primary key
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
