b741e8a2d2d4f57da52eec44091d4e6a6a6cff9b
vladyslav-keidaliuk <vladislavdocuments@gmail.com>
2023-10-30 23:36:20 +0200

    Added Views for Product and add a Controller

---
+++ CourseWorkWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using CourseWorkWeb.Data;
+using CourseWorkWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseWorkWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        // private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> ProductList = _unitOfWork.Product.GetAll().ToList();
+
+            return View(ProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            // Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            // Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            // if (product.Name == product.DisplayOrder.ToString())
+            // {
+            //     ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            // }
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            // Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            // Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? product = _unitOfWork.Product.Get(u => u.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
