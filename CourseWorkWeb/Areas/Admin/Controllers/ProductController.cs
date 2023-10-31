3b478207ccf60e84fc54f36828d25dbbe63a8c17
vladyslav-keidaliuk <vladislavdocuments@gmail.com>
2023-11-01 00:46:51 +0200

    Combine Create and Update views in 1. Change ViewBag on ViewModels. Now when you try to update product you can see a small image

--- CourseWorkWeb/Areas/Admin/Controllers/ProductController.cs
+++ CourseWorkWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using CourseWorkWeb.Data;
 using CourseWorkWeb.Models;
+using CourseWorkWeb.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -10,10 +11,12 @@ namespace CourseWorkWeb.Areas.Admin.Controllers
     {
         // private readonly IProductRepository _productRepository;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -23,26 +26,73 @@ namespace CourseWorkWeb.Areas.Admin.Controllers
             return View(ProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
-                .GetAll().Select(u => new SelectListItem
+            ProductVM productVm = new()
+            {
+                CategoryList = _unitOfWork.Category
+                    .GetAll().Select(u => new SelectListItem
                     {
                         Text = u.Name,
                         Value = u.Id.ToString()
-                    }
-                );
-            ViewBag.CategoryList = CategoryList;
+                    }),
+                Product = new Product()
+            };
+            if (id == null || id == 0)
+            {
+                //create
+                return View(productVm);
+            }
+            else
+            {
+                //update
+                productVm.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVm);
+            }
 
-            return View();
         }
 
         [HttpPost]
-        public IActionResult Create(Product product)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(product);
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file!=null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        //delete the old image
+                        var OldImagePath = 
+                            Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(OldImagePath))
+                        {
+                            System.IO.File.Delete(OldImagePath);
+                        }
+
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath,fileName),FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
+                
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
@@ -52,43 +102,45 @@ namespace CourseWorkWeb.Areas.Admin.Controllers
 
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
+        // public IActionResult Edit(int? id)
+        // {
+        //     if (id == null || id == 0)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+        //     // Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+        //     // Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+        //
+        //     if (productFromDb == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     return View(productFromDb);
+        // }
 
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            // Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
-            // Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
 
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
 
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            // if (product.Name == product.DisplayOrder.ToString())
-            // {
-            //     ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
-            // }
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(product);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-
-            return View();
-
-        }
+        // [HttpPost]
+        // public IActionResult Edit(Product product)
+        // {
+        //     // if (product.Name == product.DisplayOrder.ToString())
+        //     // {
+        //     //     ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+        //     // }
+        //
+        //     if (ModelState.IsValid)
+        //     {
+        //         _unitOfWork.Product.Update(product);
+        //         _unitOfWork.Save();
+        //         TempData["success"] = "Product updated successfully";
+        //         return RedirectToAction("Index");
+        //     }
+        //
+        //     return View();
+        //
+        // }
 
 
         public IActionResult Delete(int? id)
